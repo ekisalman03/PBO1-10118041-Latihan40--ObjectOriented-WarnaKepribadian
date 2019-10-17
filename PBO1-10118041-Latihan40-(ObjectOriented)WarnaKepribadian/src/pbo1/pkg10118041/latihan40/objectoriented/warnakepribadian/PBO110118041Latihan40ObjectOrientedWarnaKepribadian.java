3581e83e4974101570d141e0f51765c7627035ba
ekisalman03 <55838966+ekisalman03@users.noreply.github.com>
2019-10-17 22:47:06 +0700

    Add files via upload

---
+++ PBO1-10118041-Latihan40-(ObjectOriented)WarnaKepribadian/src/pbo1/pkg10118041/latihan40/objectoriented/warnakepribadian/PBO110118041Latihan40ObjectOrientedWarnaKepribadian.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo1.pkg10118041.latihan40.objectoriented.warnakepribadian;
+
+/**
+ *
+ * @author 
+ * NAMA     : Eki salman sya'bani
+ * KELAS    : IF-3
+ * NIM      : 10118041
+ * Deskripsi Program : Program ini untuk menampilkan tampilan dari warna dan 
+ *                     kepribadian.
+ */
+public class PBO110118041Latihan40ObjectOrientedWarnaKepribadian {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        Warna skuyMu = new Warna();
+        
+        skuyMu.ioWarnaMu();
+    }
+    
+}
